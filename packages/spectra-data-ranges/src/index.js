fe19e5a94438d31a431315a12d51037022251e3d
jobo322 <jose.bolanos@correounivalle.edu.co>
2017-05-18 10:13:01 +0200

    added spectra-data-ranges package

---
+++ packages/spectra-data-ranges/src/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const defaultOptions = {};
+
+function spectraDataRanges(options) {
+    options = Object.assign({}, defaultOptions, options);
+}
+
+module.exports = spectraDataRanges;
