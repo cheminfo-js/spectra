86332fa5a687d3e80407c50d66ce3251c5829fdd
jobo322 <jose.bolanos@correounivalle.edu.co>
2017-05-23 13:12:05 +0200

    added spectra-utilities

---
+++ packages/spectra-utilities/src/index.js
@@ -0,0 +1,151 @@
+'use strict';
+
+const patterns = ['s', 'd', 't', 'q', 'quint', 'h', 'sept', 'o', 'n'];
+
+module.exports.nmrJ = function nmrJ(Js, options = {}) {
+    var jString = '';
+    options = Object.assign({}, {separator: ', ', nbDecimal: 2}, options);
+    let j, i;
+    for (i = 0; i < Js.length; i++) {
+        j = Js[i];
+        if (j.length > 11) {
+            j += options.separator;
+        }
+        jString += j.multiplicity + ' ' + j.coupling.toFixed(options.nbDecimal);
+    }
+    return jString;
+}
+
+module.exports.Coupling = function joinCoupling(signal, tolerance = 0.05) {
+    var jc = signal.j;
+    var cont = jc[0].assignment.length;
+    var pattern = '';
+    var newNmrJs = [];
+    var diaIDs = [];
+    var atoms = [];
+    if (jc && jc.length > 0) {
+        jc.sort(function (a, b) {
+            return b.coupling - a.coupling;
+        });
+        if (jc[0].diaID) {
+            diaIDs = [jc[0].diaID];
+        }
+        if (jc[0].assignment) {
+            atoms = jc[0].assignment;
+        }
+        for (var i = 0; i < jc.length - 1; i++) {
+            if (Math.abs(jc[i].coupling - jc[i + 1].coupling) < tolerance) {
+                cont += jc[i + 1].assignment.length;
+                diaIDs.push(jc[i].diaID);
+                atoms = atoms.concat(jc[i + 1].assignment);
+            } else {
+                let jTemp = {
+                    coupling: Math.abs(jc[i].coupling),
+                    multiplicity: patterns[cont]
+                };
+                if (diaIDs.length > 0) {
+                    jTemp.diaID = diaIDs;
+                }
+                if (atoms.length > 0) {
+                    jTemp.assignment = atoms;
+                }
+                newNmrJs.push(jTemp);
+                if (jc[0].diaID) {
+                    diaIDs = [jc[i].diaID];
+                }
+                if (jc[0].assignment) {
+                    atoms = jc[i].assignment;
+                }
+                pattern += patterns[cont];
+                cont = jc[i + 1].assignment.length;
+            }
+        }
+        let jTemp = {
+            coupling: Math.abs(jc[i].coupling),
+            multiplicity: patterns[cont]
+        };
+        if (diaIDs.length > 0) {
+            jTemp.diaID = diaIDs;
+        }
+        if (atoms.length > 0) {
+            jTemp.assignment = atoms;
+        }
+        newNmrJs.push(jTemp);
+
+        pattern += patterns[cont];
+        signal.j = newNmrJs;
+
+    } else if (signal.delta) {
+        pattern = 's';
+    } else {
+        pattern = 'm';
+    }
+    return pattern;
+}
+
+module.exports.group = function group(signals, options = {}) {
+    var i, k;
+    for (i = 0; i < signals.length; i++) {
+        var j = signals[i].j;
+        if (j && j.length > 0) {
+            //It is supposed that multiplicity is always `d`
+            for (k = j.length - 2; k >= 0; k--) {
+                for (var m = j.length - 1; m > k; m--) {
+                    if (j[k].diaID === j[m].diaID &&
+                        j[k].coupling === j[m].coupling &&
+                        j[k].distance === j[m].distance) {
+                        j[k].assignment = j[k].assignment.concat(j[m].assignment);
+                        j.splice(m, 1);
+                    }
+                }
+            }
+        }
+    }
+    signals.sort((a, b) => {
+        if (a.diaIDs[0] < b.diaIDs[0]) return -1;
+        if (a.diaIDs[0] > b.diaIDs[0]) return 1;
+        return 0;
+    });
+
+    for (i = signals.length - 2; i >= 0; i--) {
+        if (signals[i].diaIDs[0] === signals[i + 1].diaIDs[0]) {
+            signals[i].nbAtoms += signals[i + 1].nbAtoms;
+            signals[i].atomIDs = signals[i].atomIDs.concat(signals[i + 1].atomIDs);
+            signals.splice(i + 1, 1);
+        }
+    }
+
+    for (i = 0; i < signals.length; i++) {
+        j = signals[i].j;
+        for (k = 0; k < j.length; k++) {
+            j[k].multiplicity = patterns[j[k].assignment.length];
+        }
+        signals[i].multiplicity = compilePattern(signals[i], options.tolerance);
+    }
+    return signals;
+};
+
+
+function compilePattern(signal, tolerance = 0.05) {
+    var jc = signal.j;
+    var cont = jc[0].assignment.length;
+    var pattern = '';
+    if (jc && jc.length > 1) {
+        jc.sort(function (a, b) {
+            return b.coupling - a.coupling;
+        });
+        for (var i = 0; i < jc.length - 1; i++) {
+            if (Math.abs(jc[i].coupling - jc[i + 1].coupling) < tolerance) {
+                cont += jc[i + 1].assignment.length;
+            } else {
+                pattern += patterns[cont];
+                cont = jc[i + 1].assignment.length;
+            }
+        }
+        pattern += patterns[cont];
+    } else {
+        pattern = patterns[cont];
+    }
+    return pattern;
+}
+
---
+++ packages/spectra-utilities/test/test.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const spectraUtilities = require('..');
+
+describe('spectra-utilities test', function () {
+
+    it('Something to test', function () {
+        (true).should.be.equal(true);
+    });
+});
