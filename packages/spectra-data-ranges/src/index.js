0dd5d9dcf35751dc6f997d20d8e06785567e1602
Michaël Zasso <targos@protonmail.com>
2017-06-12 14:19:47 +0200

    fix eslint

--- packages/spectra-data-ranges/src/index.js
+++ packages/spectra-data-ranges/src/index.js
@@ -1,8 +1,7 @@
 'use strict';
 
-
-function spectraDataRanges(options = {}) {
-
+function spectraDataRanges() {
+    // todo implement
 }
 
 module.exports = spectraDataRanges;
--- packages/spectra-data-ranges/test/test.js
+++ packages/spectra-data-ranges/test/test.js
@@ -1,7 +1,5 @@
 'use strict';
 
-const spectraDataRanges = require('..');
-
 describe('spectra-data-ranges test', function () {
 
     it('Something to test', function () {
