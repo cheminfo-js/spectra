38f5a15b8bd21d5b59ab552dffe37efc4c7c6bc4
Jose Alejandro Bolanos Arroyave <jose.bolanos@correounivalle.edu.co>
2017-11-27 14:52:25 -0500

    fix updateSignalIntegral

--- packages/spectra-nmr-utilities/src/updateSignalIntegral.js
+++ packages/spectra-nmr-utilities/src/updateSignalIntegral.js
@@ -2,7 +2,7 @@ export default function updateSignalIntegral(signals, totalIntegral) {
     let sum = signals.reduce((a, b) => a + b.integralData.value, 0);
     let factor = totalIntegral / sum;
     return signals.map((s) => {
-        s.integralData.value * factor;
+        s.integralData.value *= factor;
         return s;
     });
 }
