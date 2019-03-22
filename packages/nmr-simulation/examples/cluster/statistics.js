c8fb842121645e00a1290120da9d38fb6487f52a
andcastillo <andcastillo@gmail.com>
2019-03-22 14:17:33 -0500

    enhance statistics script

--- packages/nmr-simulation/examples/cluster/statistics.js
+++ packages/nmr-simulation/examples/cluster/statistics.js
@@ -18,61 +18,70 @@ var options = {
 }
 
 var filter = { filter: '.json' };
-var path = `${__dirname}/spinus/`;
-var spinus = FS.readdirSync(path).filter((line) => {
+var path = '/home/acastillo/Documents/data/data/';
+var spinus = FS.readdirSync(path + 'spinus/').filter((line) => {
   return line.indexOf(filter.filter) > 0;
 });
 
 
 console.log("n molecules " + spinus.length)
-let maxSize = 15;
-let histogram = [];
-for(let i = 0; i <= maxSize; i++) {
-  histogram[i] = [];
-}
-for (var p = 0; p < spinus.length; p++) {
-  let prediction = JSON.parse(loadFile(path + spinus[p]));
-  const spinSystem = sm.SpinSystem.fromPrediction(prediction);    //console.log(spinSystem);
-  let betas = spinSystem._calculateBetas(spinSystem.couplingConstants, options.frequency);
-  for (let size = 7; size <= maxSize; size++) {
+let maxSize = 9;
+
+for (let size = 9; size <= maxSize; size++) {
+  let histogram = [];
+  for (let i = 0; i <= size; i++) {
+    histogram[i] = [];
+  }
+  let countFailures = 0;
+  for (var p = 0; p < spinus.length; p++) {
+    let prediction = JSON.parse(loadFile(path + 'spinus/' + spinus[p]));
+    const spinSystem = sm.SpinSystem.fromPrediction(prediction);    //console.log(spinSystem);
+    let betas = spinSystem._calculateBetas(spinSystem.couplingConstants, options.frequency);
     options.maxClusterSize = size;
     spinSystem.ensureClusterSize(options);
     let clusters = spinSystem.clusters;
     clusters.forEach(value => {
       if (value.length <= size) {
         for (let i = 0; i < value.length; i++) {
-          if(value[i] < 0) {
-            histogram[value.length].push(maxBeta(value, betas));
+          if (value[i] < 0) {
+            let beta = maxBeta(value, betas);
+            histogram[value.length].push(beta);
+            if(beta > 5) {
+              console.log(path + 'spinus/' + spinus[p] + " " + beta);
+            }
             break;
           }
         }
       }
       else {
-        console.log("X " + value.length);
+        countFailures++;
+        //console.log("X " + value.length);
       }
     });
   }
+  for (let i = 0; i <= size; i++) {
+    histogram[i] = getStats(histogram[i]);
+  }
+  histogram[0] = {countFailures};
+  FS.writeFileSync(path + '/clusterSize' + size + '.json', JSON.stringify(histogram));
 }
-for(let i = 0; i <= maxSize; i++) {
-  histogram[i] = getStats(histogram[i]);
-  console.log(histogram[i]);
-}
+
 
 function maxBeta(value, betas) {
   let max = 0;
-  for (let i = 0; i < value.length; i++ ) {
+  for (let i = 0; i < value.length; i++) {
     for (let j = i + 1; j < value.length; j++) {
-      if(value[i] >= 0) {
-        if(value[j] < 0 ) {
-          max = Math.max(max, beta = 1-betas[value[i]][-value[j] - 1]);
+      if (value[i] >= 0) {
+        if (value[j] < 0) {
+          max = Math.max(max, beta = 1 - betas[value[i]][-value[j] - 1]);
         }
       } else {
-        if(value[j] >= 0 ) {
-          max = Math.max( max, beta = 1-betas[-value[i] - 1][value[j]]);
+        if (value[j] >= 0) {
+          max = Math.max(max, beta = 1 - betas[-value[i] - 1][value[j]]);
         }
       }
     }
-  } 
+  }
   return max;
 }
 
